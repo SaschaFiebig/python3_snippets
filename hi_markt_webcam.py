9fc94c75ef5651f32364b01cbe8402cfa7a2740b
simcup <cheff@simcup.de>
2023-10-22 14:40:05 +0200

    added some random delay to each pause so the probability of hitting an overboted server is lowered

--- hi_markt_webcam.py
+++ hi_markt_webcam.py
@@ -1,12 +1,14 @@
 import os 
 import time 
 from colorama import Fore, Back, Style 
+import random
 
 
 s_download_url = 'http://rathaus-hildesheim.de/webcam/webcam.jpg'
 s_prefix = 'hildesheim_marktplatz_'
 s_suffix = '.jpg'
 i_pause_in_seconds = 60
+i_delay = 0
 
 print(Back.RED + Fore.BLACK + '---{ STARTING DOWNLOAD }---' + Style.RESET_ALL + "\n\n")
 
@@ -18,13 +20,17 @@ while(True):
     # assemble wget command 
     s_wget_command = f"wget -O {s_filename} --user-agent='Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0' {s_download_url}"
 
+    # delay execution by random amount to hopefully prevent overBoting the server and getting to much 404s
+    i_delay = random.randint(1,8)
+    time.sleep(i_delay)
+    
     # print progress
     print(Fore.RED + f"Download file: {s_filename}")
     print(f"From: {s_download_url}" + Style.RESET_ALL)
 
     # execute wget command 
     os.system(s_wget_command)
-    time.sleep(i_pause_in_seconds)
+    time.sleep(i_pause_in_seconds-i_delay)
     print("\n")
 
 print(Back.RED + Fore.BLACK + '---{ DOWNLOAD COMPLETE }---' + Style.RESET_ALL)
